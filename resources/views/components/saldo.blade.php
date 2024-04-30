80b36ed8a4487a328767ad82d559e5e1a2af0ace
Rakai Seto <mamangrakai@gmail.com>
2024-04-30 17:58:57 +0700

    feat : using pic

--- resources/views/components/saldo.blade.php
+++ resources/views/components/saldo.blade.php
@@ -23,52 +23,52 @@
             <!-- Card 1 End  -->
         </div>
 
-        <div class="col-xxl-6 col-sm-6 mb-25">
-            <!-- Card 2 -->
-            <div class="ap-po-details ap-po-details--2 p-25 radius-xl d-flex justify-content-between">
-                <div class="overview-content w-100">
-                    <div class=" ap-po-details-content d-flex flex-wrap justify-content-between">
-                        <div class="ap-po-details__titlebar">
-                            <h1>{{ $trx_today }}</h1>
-                            <p>Total WA hari ini by Participant</p>
-                        </div>
-                        <div class="ap-po-details__icon-area">
-                            <div class="svg-icon order-bg-opacity-info color-info">
+{{--        <div class="col-xxl-6 col-sm-6 mb-25">--}}
+{{--            <!-- Card 2 -->--}}
+{{--            <div class="ap-po-details ap-po-details--2 p-25 radius-xl d-flex justify-content-between">--}}
+{{--                <div class="overview-content w-100">--}}
+{{--                    <div class=" ap-po-details-content d-flex flex-wrap justify-content-between">--}}
+{{--                        <div class="ap-po-details__titlebar">--}}
+{{--                            <h1>{{ $trx_today }}</h1>--}}
+{{--                            <p>Total WA hari ini by Participant</p>--}}
+{{--                        </div>--}}
+{{--                        <div class="ap-po-details__icon-area">--}}
+{{--                            <div class="svg-icon order-bg-opacity-info color-info">--}}
 
-                                <i class="fa fa-calendar-day"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+{{--                                <i class="fa fa-calendar-day"></i>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
 
-            </div>
-            <!-- Card 2 End  -->
-        </div>
-
-
-        <div class="col-xxl-6 col-sm-6 mb-25">
-            <!-- Card 3 -->
-            <div class="ap-po-details ap-po-details--2 p-25 radius-xl d-flex justify-content-between">
+{{--            </div>--}}
+{{--            <!-- Card 2 End  -->--}}
+{{--        </div>--}}
 
 
-                <div class="overview-content w-100">
-                    <div class=" ap-po-details-content d-flex flex-wrap justify-content-between">
-                        <div class="ap-po-details__titlebar">
-                            <h1>{{$trx_all}}</h1>
-                            <p>Total WA all-time by Participant</p>
-                        </div>
-                        <div class="ap-po-details__icon-area">
-                            <div class="svg-icon order-bg-opacity-secondary color-secondary">
+{{--        <div class="col-xxl-6 col-sm-6 mb-25">--}}
+{{--            <!-- Card 3 -->--}}
+{{--            <div class="ap-po-details ap-po-details--2 p-25 radius-xl d-flex justify-content-between">--}}
 
-                                <i class="fa fa-calendar-week"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
-            </div>
-            <!-- Card 3 End  -->
-        </div>
+{{--                <div class="overview-content w-100">--}}
+{{--                    <div class=" ap-po-details-content d-flex flex-wrap justify-content-between">--}}
+{{--                        <div class="ap-po-details__titlebar">--}}
+{{--                            <h1>{{$trx_all}}</h1>--}}
+{{--                            <p>Total WA all-time by Participant</p>--}}
+{{--                        </div>--}}
+{{--                        <div class="ap-po-details__icon-area">--}}
+{{--                            <div class="svg-icon order-bg-opacity-secondary color-secondary">--}}
+
+{{--                                <i class="fa fa-calendar-week"></i>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+
+{{--            </div>--}}
+{{--            <!-- Card 3 End  -->--}}
+{{--        </div>--}}
         <div class="col-xxl-6 col-sm-6 mb-25">
             <!-- Card 4  -->
             <div class="ap-po-details ap-po-details--2 p-25 radius-xl d-flex justify-content-between">
